2438a343d3745d27794f664fe59abadb2acca1c6
nourbenhazem <claudiabenhazem@gmail.com>
2023-05-05 16:07:15 +0100

    fixk

--- _printf.c
+++ _printf.c
@@ -8,12 +8,9 @@
 int _printf(const char *format, ...)
 {
 	int count = 0, i;
-
 	va_list ptr;
-	/* Check if format == NULL */
 	if (format == NULL)
 		return (-1);
-
 	va_start(ptr, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
@@ -22,34 +19,24 @@ int _printf(const char *format, ...)
 			switch (format[i + 1])
 			{
 			case 's':
-				/* Move index forward to not print the 's' */
 				i++;
 				count += printString(va_arg(ptr, char *));
-				/* added break as hinted in the guide to use switch in C language */
 				break;
 			case 'c':
-				/* Move index forward to not print the 'c' */
 				i++;
 				count += printChar(va_arg(ptr, int));
-				/* added break as hinted in the guide to use switch in C language */
 				break;
 			case 'd':
-				/* Move index forward to not print the 'd' */
 				i++;
 				count += printDecimal(va_arg(ptr, int));
-				/* added break as hinted in the guide to use switch in C language */
 				break;
 			case 'i':
-				/* Move index forward to not print the 'i' */
 				i++;
 				count += printDecimal(va_arg(ptr, int));
-				/* added break as hinted in the guide to use switch in C language */
 				break;
 			case '%':
-				/* added break as hinted in the guide to use switch in C language */
 				break;
 			case '\0':
-				/* error case handled */
 				return (-1);
 			default:
 				count += printChar(format[i]);
@@ -62,4 +49,3 @@ int _printf(const char *format, ...)
 	va_end(ptr);
 	return count;
 }
-
